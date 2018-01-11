67fa8f63fc5dc26c89b717b457e46f9bfb3fa3eb
unknown <belika@hotmail.com>
2018-01-11 12:32:34 +0000

    removing temp methods

--- run.js
+++ run.js
@@ -20,6 +20,25 @@ app.use('/index.js', express.static(__dirname + '/index.js'));
 app.use('/audio', express.static(__dirname + '/audio'));
 app.use('/node_modules', express.static(__dirname + '/node_modules'));
 
+
+// app.post('/game/:name/:state/:confidence', function (req, res) {
+//   var state = req.params.state;
+//   var gameName = req.params.name;
+//   var confidence = req.params.confidence;
+
+//   Storage.put(gameName, { gameName: confidence });
+//   return res.status(200);
+// });
+
+// app.get('/game/:name/:state', function (req, res) {
+//   var state = req.params.state;
+//   var gameName = req.params.name;
+
+//   var confidence = Storage.get(gameName.state)
+  
+//   return res.json(confidence)
+// });
+
 /* GET home page. */
 app.get('*', function (req, res, next) {
   //Path to your main file
@@ -30,23 +49,6 @@ app.get('*', function (req, res, next) {
 
 });
 
-app.post('/game/:name/:state/:confidence', function (req, res) {
-  var state = req.params.state;
-  var gameName = req.params.name;
-  var confidence = req.params.confidence;
-
-  Storage.put(gameName, { gameName: confidence });
-  return res.status(200);
-});
-
-app.get('/game/:name/:state', function (req, res) {
-  var state = req.params.state;
-  var gameName = req.params.name;
-
-  var confidence = Storage.get(gameName.state)
-  
-  return res.json(confidence)
-});
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!')
