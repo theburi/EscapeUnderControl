6e592c2ef5f1bfab8a80037c4c83a11966dd978a
theburi <belika@hotmail.com>
2018-07-28 08:58:46 +0100

    Adding modbus

--- run.js
+++ run.js
@@ -9,6 +9,7 @@ var Storage = require('node-storage');
 
 var app = express();
 var store = new Storage('gamestatus.json');
+var GameState = [{ name: 'Puzzle 1', state: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0] }, { name: 'Puzzle 2', state: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0] }];
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -21,29 +22,6 @@ app.use('/index.js', express.static(__dirname + '/index.js'));
 app.use('/audio', express.static(__dirname + '/audio'));
 app.use('/node_modules', express.static(__dirname + '/node_modules'));
 
-
-app.post('/game/:name/:state/:confidence', function (req, res) {
-
-  var state = req.params.state;
-  var gameName = req.params.name;
-  var confidence = parseFloat(req.params.confidence);
-  var oldvalue = parseFloat(store.get(state));
-  var newvalue = 0
-  if (confidence > 0) {
-    
-    if (oldvalue > 0) {
-      newvalue = (oldvalue * 10 + confidence) / 11;
-    }
-    else {
-      newvalue = confidence
-    }
-
-    store.put(state, newvalue)
-  }
-  res.status(200);
-  return res.end();
-});
-
 app.post('/game/:name', function (req, res) {
   console.log('update all statuses')
   var states = ["junglecaveset", "pharaoheye", "pharaohmummy", "jungletablestart", "pharaohchest", "jungles2", "jungleset", "pharaohscarab", "jungles1rihno", "jungles1lion", "pharaohlibra", "junglecavechest", "junglecavebox", "pharaohset", "jungleend"];
@@ -51,25 +29,27 @@ app.post('/game/:name', function (req, res) {
     states.forEach(function (state) {
       store.remove(state)
     })
-
-  return
 });
 
-app.get('/game/:name/:state', function (req, res) {
-  var state = req.params.state;
-  var gameName = req.params.name;
-  if (gameName) {
-    if (store.get(state)) {
-      res.json({ 'value': store.get(state) });
-    } else {
-      res.json({ 'value': 0 });
-    }
-  }
-  return
-});
 
 app.get('/game/:name', function (req, res) {
   return
+});
+
+
+app.get('/game/:name/state', function (req, res) {
+  client.readHoldingRegisters(0, 16, function(err, data) {
+    if (err) console.log("Error", err);
+    console.log(data.data);
+  });
+  return data.data;
+});
+
+
+app.post('/game/:name/state/:puzzle', function (req, res) { 
+    client.writeRegisters ( 0, GameState.puzzle, function(err, data) {
+    if (err) console.log("Error", err);
+  })
 });
 
 /* GET home page. */
@@ -78,12 +58,21 @@ app.get('*', function (req, res, next) {
   //res.status(200).sendFile(path.join(__dirname+'main.html')); 
   console.log(__dirname);
   res.sendFile(__dirname + '/main.html');
-
 });
 
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!')
 });
+
+// ModBus
+const ModbusRTU = require("modbus-serial");
+// create an empty modbus client
+const client = new ModbusRTU();
+// open connection to a serial port
+client.connectRTUBuffered("/dev/ttyS0", { baudRate: 9600 });
+// set timeout, if slave did not reply back
+client.setTimeout(500);
+
 
 module.exports = app;
