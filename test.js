d7f2d1e6565c8e5e46f43123584ee830f775bc8c
theburi <belika@hotmail.com>
2018-07-29 11:04:43 +0100

    updates

--- test.js
+++ test.js
@@ -1,12 +1,30 @@
 // ModBus
 const ModbusRTU = require("modbus-serial");
 // create an empty modbus client
+
 const client = new ModbusRTU();
 // open connection to a serial port
-client.connectRTUBuffered("/dev/ttyS0", { baudRate: 9600 });
+client.connectRTUBuffered("/dev/ttyS0", { baudRate: 9600}).
 // set timeout, if slave did not reply back
-client.setTimeout(500);
+//client.setTimeout(500);
 
-client.writeRegisters ( 1, 100, function(err, data) {
-    if (err) console.log("Error", err);
-  });
+then(function () {
+	console.log("connected");
+	client.setID(1);
+	client.writeCoil ( 0, true)
+		.then( function(err, data) {
+    			console.log("Data: ", err, " " , data); 
+//			process.exit();
+});
+
+        client.writeCoil ( 0, false)
+                .then( function(err, data) {
+                        console.log("Data: ", err, " " , data);
+   //                     process.exit();
+                });
+
+}).catch(function(e) {
+	console.log(e.message);
+	process.exit(); 
+})
+
